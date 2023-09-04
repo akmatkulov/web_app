307009fb651347f1b20577beedabae95f94a2f5e
akmatkulov <akmatkulovs991@gmail.com>
2023-09-04 19:43:19 +0600

    Refactoring reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,12 @@
 class ReviewsController < ApplicationController
 
   before_action :find_iphone!
+  before_action :find_review!, except: :create
 
   def edit
-    @review = @iphone.reviews.find params[:id]
   end
 
   def update
-    @review = @iphone.reviews.find params[:id]
     if @review.update review_params
       flash[:success] = "Review Updated"
       redirect_to iphone_path(@iphone)
@@ -17,8 +16,7 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    review = @iphone.reviews.find params[:id]
-    review.destroy 
+    @review.destroy 
     flash[:success] = "Review Deleted"
     redirect_to iphone_path(@iphone)
   end
@@ -41,6 +39,10 @@ class ReviewsController < ApplicationController
 
     def find_iphone!
       @iphone = Iphone.find params[:iphone_id]
+    end
+
+    def find_review!
+      @review = @iphone.reviews.find params[:id]
     end
 
 end
